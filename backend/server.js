c753e4a0958b7b91f68c0887b3c74b56c239c4d1
Avinash Hanwat <41325993+ahanwat123@users.noreply.github.com>
2025-05-02 20:07:57 +0530

    cv

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use('/api/messenger',authRouter);
 app.use('/api/messenger', messengerRoute)
-console.log("klllllllllllllllllllll")
+console.log("klllllllllllllllllllllqwe")
 const PORT = process.env.PORT || 5000
 app.get('/', (req, res)=>{
     res.send("running")
